4832af8eb1695150dfeca155127f647ad961b1fc
Morgan Galvin <morgan@ludwigthepig.com>
2020-02-15 19:27:21 -0700

    fix: switch innnerWidth and innerHeight in isMobile function

--- src/controllers/inputHandler.js
+++ src/controllers/inputHandler.js
@@ -1,6 +1,6 @@
 import initializeKeyboard from './keyboardInput';
 
-const isMobile = () => ((window.innerWidth <= 800) && (window.innerHeight <= 600));
+const isMobile = () => ((window.innerHeight <= 800) && (window.innerWidth <= 600));
 
 const inputHandler = () => {
   if (isMobile()) {

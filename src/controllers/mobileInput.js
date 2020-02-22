25aa1ed6fa1daefb504640106e27884eca3a4ba8
Morgan Galvin <morgan@ludwigthepig.com>
2020-02-21 22:46:16 -0700

    feat: add basic swipe detection!

--- src/controllers/mobileInput.js
+++ src/controllers/mobileInput.js
@@ -1,7 +1,21 @@
-const handleStart = e => console.log('handle start', e);
+const getTouchPos = e => ({ x: e.touches[0].screenX, y: e.touches[0].screenY });
+
+let origin;
+const handleStart = e => {
+  origin = getTouchPos(e);
+};
 const handleEnd = e => console.log('handle end', e);
 const handleCancel = e => console.log('handle cancel', e);
-const handleMove = e => console.log('handle move', e);
+const handleMove = e => {
+  const { x, y } = getTouchPos(e);
+
+  if (origin.x < x) console.log('right!');
+  else console.log('left!');
+  console.log('handle move', e);
+
+  if (origin.y < y) console.log('down!');
+  else console.log('up!');
+};
 
 const initializeMobile = () => {
   const el = document.getElementById('canvas-container').children[0];

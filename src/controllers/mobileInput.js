c757071899657c84eaeec8b413c6e5fc25eb3b09
Morgan Galvin <morgan@ludwigthepig.com>
2020-02-21 22:56:13 -0700

    feat: add tolerance/buffer zone for touch movement

--- src/controllers/mobileInput.js
+++ src/controllers/mobileInput.js
@@ -1,30 +1,38 @@
 import { inputState } from './movement';
 
 const getTouchPos = e => ({ x: e.touches[0].screenX, y: e.touches[0].screenY });
-
 let origin;
+const tolerance = 100;
 const handleStart = e => {
   origin = getTouchPos(e);
 };
 const handleEnd = e => console.log('handle end', e);
 const handleCancel = e => console.log('handle cancel', e);
+
+
 const handleMove = e => {
   const { x, y } = getTouchPos(e);
 
-  if (origin.x < x) {
+  if (origin.x < x - tolerance) {
     inputState.right = true;
     inputState.left = false;
-  } else {
+  } else if (origin.x > x + tolerance) {
     inputState.right = false;
     inputState.left = true;
+  } else {
+    inputState.right = false;
+    inputState.left = false;
   }
 
-  if (origin.y < y) {
+  if (origin.y < y - tolerance) {
     inputState.down = true;
     inputState.up = false;
-  } else {
+  } else if (origin.y > y + tolerance) {
     inputState.down = false;
     inputState.up = true;
+  } else {
+    inputState.up = false;
+    inputState.down = false;
   }
 };
 

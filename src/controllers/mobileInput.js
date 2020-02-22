4bdec1144f2e2ab91c5add96e096fb62e101dac0
Morgan Galvin <morgan@ludwigthepig.com>
2020-02-21 22:20:04 -0700

    feat: add my first touch event :)

--- src/controllers/mobileInput.js
+++ src/controllers/mobileInput.js
@@ -1,5 +1,14 @@
-const initializeMobile = () => {
+const handleStart = e => console.log('handle start', e);
+const handleEnd = e => console.log('handle end', e);
+const handleCancel = e => console.log('handle cancel', e);
+const handleMove = e => console.log('handle move', e);
 
+const initializeMobile = () => {
+  const el = document.getElementById('canvas-container').children[0];
+  el.addEventListener('touchstart', handleStart, false);
+  el.addEventListener('touchend', handleEnd, false);
+  el.addEventListener('touchcancel', handleCancel, false);
+  el.addEventListener('touchmove', handleMove, false);
 };
 
 export default initializeMobile;

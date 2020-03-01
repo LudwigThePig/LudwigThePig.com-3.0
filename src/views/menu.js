36864e47986fd392626a5bcd8ca4666b6efc2540
Morgan Galvin <morgan@ludwigthepig.com>
2020-02-29 19:59:35 -0700

    feat: add js controllers to button

--- src/views/menu.js
+++ src/views/menu.js
@@ -1,5 +1,26 @@
+class MenuHandler {
+  constructor(toggler) {
+    this.isActive = false;
+    this.toggler = toggler;
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    if (this.isActive)
+      this.toggler.classList.remove('is-active');
+    else
+      this.toggler.classList.add('is-active');
+    this.isActive = !this.isActive;
+  }
+}
+
+
 const init = () => {
+  const toggleMenuButton = document.getElementById('menu-button');
+  const menuHandler = new MenuHandler(toggleMenuButton);
+
   document.getElementsByClassName('ui');
+  toggleMenuButton.addEventListener('click', menuHandler.toggleMenu);
 };
 
 export default init;

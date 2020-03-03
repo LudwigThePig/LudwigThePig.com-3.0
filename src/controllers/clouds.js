0cc8c8c050d7565e598f8186979dcdffb539e0d8
Morgan Galvin <morgan@ludwigthepig.com>
2020-03-02 19:32:10 -0700

    feat: add random noise to cloud movement

--- src/controllers/clouds.js
+++ src/controllers/clouds.js
@@ -3,7 +3,11 @@ import { easeInOutSine } from '../utils/easing';
 let direction = 1;
 let time = 0;
 const totalDuration = 15000;
-const maxSpeed = 0.003;
+const maxSpeed = 0.002;
+
+
+const xNoise = new Array(20).fill('').map(() => (1 + (Math.random() - 0.5)));
+const zNoise = new Array(20).fill('').map(() => (1 + (Math.random() - 0.5)));
 
 const updateCloudsPosition = clouds => {
   time += 1;
@@ -13,9 +17,9 @@ const updateCloudsPosition = clouds => {
   }
   const speed = (easeInOutSine(time, 0.001, maxSpeed, totalDuration / 2));
 
-  clouds.forEach(cloud => {
-    cloud.position.x += speed * direction;
-    cloud.position.z += speed * direction;
+  clouds.forEach((cloud, i) => {
+    cloud.position.x += speed * direction * xNoise[i];
+    cloud.position.z += speed * direction * zNoise[i];
   });
 };
 

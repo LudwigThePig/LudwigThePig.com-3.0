b923c46457cabb0ac7cbd3b83597e4c9f18f6038
Morgan Galvin <morgan@ludwigthepig.com>
2020-08-02 14:40:37 -0600

    feat: easy curvy

--- src/utils/easing.js
+++ src/utils/easing.js
@@ -1,4 +1,16 @@
-// eslint-disable-next-line import/prefer-default-export
 export const easeInOutSine = (
   elapsed, initialValue, amountOfChange, duration,
 ) => (-amountOfChange / 2) * (Math.cos(Math.PI * elapsed / duration) - 1) + initialValue;
+
+export const easeInExpo = (
+  elapsed, initialValue, amountOfChange, duration,
+) => (elapsed === 0 ? initialValue : amountOfChange * (2 ** (10 * (elapsed / duration - 1))) + initialValue);
+
+// goes from 0 to 1 to 0
+export const fullSineEase = (
+  elapsed, initialValue, amountOfChange, duration,
+) => {
+  const percentage = Math.sin(Math.PI * (elapsed / duration));
+  const scalar = initialValue + (amountOfChange - initialValue);
+  return percentage * scalar;
+};
--- src/views/menuBackground.js
+++ src/views/menuBackground.js
@@ -1,30 +1,41 @@
 import { randomArrayItem } from '../utils/random';
+import { easeInOutSine, fullSineEase, easeInExpo } from '../utils/easing';
+
+const MAX_AGE = 2000;
 
 class Particle {
-  constructor(el, left = 0, top = 0) {
-    this.element = el;
-    this.x = left;
-    this.y = top;
-    this.r = 0;
+  constructor(el, height = 0, width = 0) {
     this.t = 0;
+    this.element = el;
+    this.height = el.height;
+    this.width = el.width;
+    this.maxY = 0;
+    this.maxX = width;
+    this.x = 0; // current x, also lower bound
+    this.y = height; // current y, also lower bound
+    this.r = 0;
+    this.scale = 0.1;
+
     this.update = this.update.bind(this);
     this.draw = this.draw.bind(this);
   }
 
   update(dt) {
     this.t += dt;
-    this.x += 2;
-    this.y -= 2;
+    this.x = easeInOutSine(this.t, 0, this.maxX, MAX_AGE);
+    this.y = this.height - easeInExpo(this.t, 0, this.height, MAX_AGE);
+    this.scale = fullSineEase(this.t, 0, 1, MAX_AGE);
     this.r -= (Math.PI / 20) % (Math.PI * 2); // 40 frames per rotation
   }
 
   draw(ctx) {
     ctx.setTransform(
-      1, 0, 0, // horiz scale, vert skew, horz, skew
-      1, this.x, this.y, // vert scale, horiz translation, vert translation
+      this.scale, 0, 0, // horiz scale, vert skew, horz, skew
+      this.scale, this.x, this.y, // vert scale, horiz translation, vert translation
     );
     ctx.rotate(this.r);
-    ctx.drawImage(this.element, 0, 0, 30, 30);
+
+    ctx.drawImage(this.element, -this.width / 2, -this.height / 2);
   }
 }
 
@@ -37,11 +48,20 @@ const initBackground = () => {
   const particles = [];
   let prevDate = Date.now();
   const animate = () => {
+    // remove old particles
+    if (particles.length) {
+      let i = 0;
+      while (particles[i++].t >= MAX_AGE) {}
+      particles.splice(0, i - 1); // cull excessive elements
+    }
+
+
     // Calc Delta Time for Update Function
     const curDate = Date.now();
     const dt = curDate - prevDate;
     prevDate = curDate;
     // Clear Canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, width, height);
     // Draw Static Items
     // Update and draw dynamic items
@@ -56,13 +76,11 @@ const initBackground = () => {
   const spawnAndCull = () => {
     const clouds = [document.getElementById('img-cloud1'), document.getElementById('img-cloud2')];
     const newSrc = randomArrayItem(clouds);
-    const newParicle = new Particle(newSrc, 0, height);
-    particles.unshift(newParicle);
-
-    particles.splice(15); // cull excessive elements
+    const newParicle = new Particle(newSrc, height, width);
+    particles.push(newParicle);
   };
 
-  setInterval(spawnAndCull, 1000);
+  setInterval(spawnAndCull, 300);
   requestAnimationFrame(animate);
 };
 

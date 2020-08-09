925e48fb169d8d86aee107b75b084a5504bcbbe0
Morgan Galvin <morgan@ludwigthepig.com>
2020-08-08 21:35:32 -0600

    feat: about button works, and some fixes

--- src/views/menu.js
+++ src/views/menu.js
@@ -1,31 +1,50 @@
 import initBackground from './menuBackground';
 
+const HIDDEN_CLASS = 'hidden';
 /**
  * @param {boolean} showMenu if true, will blur game and show menu
  * if false, will hide menu and unblur game
  */
-const toggleMenu = (showMenu = false) => e => {
-  const menu = document.querySelector('#menu-ctr');
-  const hud = document.querySelector('#hud-ctr');
-  const canvasContainer = document.querySelector('#canvas-container');
+const toggleMenu = (showMenu = false) => (e) => {
+  const menu = document.getElementById('menu-ctr');
+  const hud = document.getElementById('hud-ctr');
+  const canvasContainer = document.getElementById('canvas-container');
   if (showMenu) {
     canvasContainer.classList.add('blur');
-    hud.classList.add('hidden');
-    menu.classList.remove('hidden');
+    hud.classList.add(HIDDEN_CLASS);
+    menu.classList.remove(HIDDEN_CLASS);
   } else {
     canvasContainer.classList.remove('blur');
-    hud.classList.remove('hidden');
-    menu.classList.add('hidden');
+    hud.classList.remove(HIDDEN_CLASS);
+    menu.classList.add(HIDDEN_CLASS);
   }
 };
 
+const toggleAbout = menuState => e => {
+  const getSectionEl = section => document.getElementById(`menu-section-${section}`);
+  const mainEl = getSectionEl('main');
+  const aboutEl = getSectionEl('about');
+  if (menuState.isShowingAbout) {
+    aboutEl.classList.add(HIDDEN_CLASS);
+    mainEl.classList.remove(HIDDEN_CLASS);
+  } else {
+    aboutEl.classList.remove(HIDDEN_CLASS);
+    mainEl.classList.add(HIDDEN_CLASS);
+  }
+  menuState.isShowingAbout = !menuState.isShowingAbout;
+};
 
 const init = () => {
+  const menuState = {
+    isShowingAbout: false,
+  };
   const startGameButton = document.getElementById('start-game-button');
   const showMenuButton = document.getElementById('show-menu-button');
+  const showAboutButtons = document.querySelectorAll('.toggle-about-button');
 
   startGameButton.addEventListener('click', toggleMenu(false));
   showMenuButton.addEventListener('click', toggleMenu(true));
+  showAboutButtons.forEach(el => el.addEventListener('click', toggleAbout(menuState)));
 
   initBackground();
 };
